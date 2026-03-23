946955d79d7c9b64dca3a84a0d1294fa34ec02f3
Hardhik-Poosa <hardhikpusa@gmail.com>
2026-03-23 23:23:37 +0530

    final push

--- collect_imgs.py
+++ collect_imgs.py
@@ -52,7 +52,7 @@ for letter in alphabet_classes:
         cv2.imshow('frame', frame)
         cv2.waitKey(25)
         cv2.imwrite(os.path.join(class_dir, f'{counter}.jpg'), frame)
-        counter += 0
+        counter += 1
 
 cap.release()
 cv2.destroyAllWindows()
